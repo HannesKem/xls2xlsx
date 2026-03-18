909562f9f3a5eace7fb99c2f107919505ffb51cc
HannesKem <hannes.kemner@bertelsmann.de>
2026-03-18 09:44:19 +0100

    improve performance

--- xls2xlsx/xls2xlsx.py
+++ xls2xlsx/xls2xlsx.py
@@ -18,6 +18,31 @@ except Exception:
 
 RETRIES=6
 
+FILL_PATTERNS = {
+    0x00: 'none', 0x01: 'solid', 0x02: 'mediumGray', 0x03: 'darkGray', 0x04: 'lightGray',
+    0x05: 'darkHorizontal', 0x06: 'darkVertical', 0x07: 'darkDown', 0x08: 'darkUp', 0x09: 'darkGrid',
+    0x0A: 'darkTrellis', 0x0B: 'lightHorizontal', 0x0C: 'lightVertical', 0x0D: 'lightDown', 0x0E: 'lightUp',
+    0x0F: 'lightGrid', 0x10: 'lightTrellis', 0x11: 'gray125', 0x12: 'gray0625'
+}
+HORIZONTAL_ALIGNMENTS = {
+    0: 'general', 1: 'left', 2: 'center', 3: 'right', 4: 'fill', 5: 'justify', 6: 'centerContinuous', 7: 'distributed'
+}
+VERTICAL_ALIGNMENTS = {
+    0: 'top', 1: 'center', 2: 'bottom', 3: 'justify', 4: 'distributed'
+}
+BORDER_STYLES = {
+    0: None, 1: 'thin', 2: 'medium', 3: 'dashed', 4: 'dotted',
+    5: 'thick', 6: 'double', 7: 'hair', 8: 'mediumDashed', 9: 'dashDot',
+    10: 'mediumDashDot', 11: 'dashDotDot', 12: 'mediumDashDotDot',
+    13: 'slantDashDot',
+}
+
+DEFAULT_FONT = Font()
+DEFAULT_FILL = PatternFill()
+DEFAULT_BORDER = Border()
+DEFAULT_ALIGNMENT = Alignment()
+DEFAULT_PROTECTION = Protection(locked=False, hidden=False)
+
 class XLS2XLSX:
     """Convert an xls file into an xlsx file.  Everything is supported except for the things
        not supported by xlrd, which include:
@@ -32,6 +57,8 @@ class XLS2XLSX:
         """f is a url, filename, file object, or xls file contents as a bytes object"""
         self.dirname = dirname
         self.ignore_workbook_corruption = ignore_workbook_corruption
+        self._color_cache = {}
+        self._style_cache = {}
         if isinstance(f, bytes):
             self.contents = f
         else:
@@ -88,11 +115,16 @@ class XLS2XLSX:
         return datetime(date_tuple[0], date_tuple[1], date_tuple[2], date_tuple[3], date_tuple[4], date_tuple[5])
 
     def xls_color_to_xlsx(self, color_ndx):
+        if color_ndx in self._color_cache:
+            return self._color_cache[color_ndx]
+
         black = (0, 0, 0)
         color_tuple = self.book.colour_map.get(color_ndx, black)
         if color_tuple is None:
             color_tuple = black
-        return Color(f'{color_tuple[0]:02X}{color_tuple[1]:02X}{color_tuple[2]:02X}')
+        color = Color(f'{color_tuple[0]:02X}{color_tuple[1]:02X}{color_tuple[2]:02X}')
+        self._color_cache[color_ndx] = color
+        return color
 
     def xls_width_to_xlsx(self, width):
         return width / 256      # to chars
@@ -102,6 +134,9 @@ class XLS2XLSX:
 
     def xls_style_to_xlsx(self, xf_ndx):
         """Convert an xls xf_ndx into a 6-tuple of styles for xlsx"""
+        if xf_ndx in self._style_cache:
+            return self._style_cache[xf_ndx]
+
         font = Font()
         fill = PatternFill()
         border = Border()
@@ -139,24 +174,17 @@ class XLS2XLSX:
                 protection.locked = xf.protection.cell_locked
             protection.hidden = xf.protection.formula_hidden
 
-            fill_patterns = {0x00:'none', 0x01:'solid', 0x02:'mediumGray', 0x03:'darkGray', 0x04:'lightGray', 
-                    0x05:'darkHorizontal', 0x06:'darkVertical', 0x07:'darkDown', 0x08:'darkUp', 0x09:'darkGrid', 
-                    0x0A:'darkTrellis', 0x0B:'lightHorizontal', 0x0C:'lightVertical', 0x0D:'lightDown', 0x0E:'lightUp',
-                    0x0F:'lightGrid', 0x10:'lightTrellis', 0x11:'gray125', 0x12:'gray0625'
-                    }
             fill_pattern = xf.background.fill_pattern
             fill_background_color = self.xls_color_to_xlsx(xf.background.background_colour_index)
             fill_pattern_color = self.xls_color_to_xlsx(xf.background.pattern_colour_index)
-            fill.patternType = fill_patterns.get(fill_pattern, 'none')
+            fill.patternType = FILL_PATTERNS.get(fill_pattern, 'none')
             fill.bgColor = fill_background_color
             fill.fgColor = fill_pattern_color
 
-            horizontal = {0:'general', 1:'left', 2:'center', 3:'right', 4:'fill', 5:'justify', 6:'centerContinuous', 7:'distributed'}
-            vertical = {0:'top', 1:'center', 2:'bottom', 3:'justify', 4:'distributed'}
-            hor_align = horizontal.get(xf.alignment.hor_align, None)
+            hor_align = HORIZONTAL_ALIGNMENTS.get(xf.alignment.hor_align, None)
             if hor_align:
                 alignment.horizontal = hor_align
-            vert_align = vertical.get(xf.alignment.vert_align, None)
+            vert_align = VERTICAL_ALIGNMENTS.get(xf.alignment.vert_align, None)
             if vert_align:
                 alignment.vertical = vert_align
             alignment.textRotation = xf.alignment.rotation
@@ -164,16 +192,12 @@ class XLS2XLSX:
             alignment.indent = xf.alignment.indent_level
             alignment.shrink_to_fit = xf.alignment.shrink_to_fit
 
-            border_styles = {0: None, 1:'thin', 2:'medium', 3:'dashed', 4:'dotted',
-                5:'thick', 6:'double', 7:'hair', 8:'mediumDashed', 9:'dashDot',
-                10:'mediumDashDot', 11:'dashDotDot', 12:'mediumDashDotDot',
-                13:'slantDashDot',}
             xls_border = xf.border
-            top = Side(style=border_styles.get(xls_border.top_line_style), color=self.xls_color_to_xlsx(xls_border.top_colour_index))
-            bottom = Side(style=border_styles.get(xls_border.bottom_line_style), color=self.xls_color_to_xlsx(xls_border.bottom_colour_index))
-            left = Side(style=border_styles.get(xls_border.left_line_style), color=self.xls_color_to_xlsx(xls_border.left_colour_index))
-            right = Side(style=border_styles.get(xls_border.right_line_style), color=self.xls_color_to_xlsx(xls_border.right_colour_index))
-            diag = Side(style=border_styles.get(xls_border.diag_line_style), color=self.xls_color_to_xlsx(xls_border.diag_colour_index))
+            top = Side(style=BORDER_STYLES.get(xls_border.top_line_style), color=self.xls_color_to_xlsx(xls_border.top_colour_index))
+            bottom = Side(style=BORDER_STYLES.get(xls_border.bottom_line_style), color=self.xls_color_to_xlsx(xls_border.bottom_colour_index))
+            left = Side(style=BORDER_STYLES.get(xls_border.left_line_style), color=self.xls_color_to_xlsx(xls_border.left_colour_index))
+            right = Side(style=BORDER_STYLES.get(xls_border.right_line_style), color=self.xls_color_to_xlsx(xls_border.right_colour_index))
+            diag = Side(style=BORDER_STYLES.get(xls_border.diag_line_style), color=self.xls_color_to_xlsx(xls_border.diag_colour_index))
             border.top = top
             border.bottom = bottom
             border.left = left
@@ -182,7 +206,17 @@ class XLS2XLSX:
             border.diagonalDown = xls_border.diag_down
             border.diagonalUp = xls_border.diag_up
 
-        return (font, fill, border, alignment, number_format, protection)
+        is_default_style = (
+            font == DEFAULT_FONT and
+            fill == DEFAULT_FILL and
+            border == DEFAULT_BORDER and
+            alignment == DEFAULT_ALIGNMENT and
+            number_format == 'General' and
+            protection == DEFAULT_PROTECTION
+        )
+        style_tuple = (font, fill, border, alignment, number_format, protection, is_default_style)
+        self._style_cache[xf_ndx] = style_tuple
+        return style_tuple
 
     def to_xlsx(self, filename=None):
         """Convert to xlsx using openpyxl.  If filename is not None, then the result
@@ -225,11 +259,24 @@ class XLS2XLSX:
 
             rows = sheet.nrows
             columns = sheet.ncols
+            error_text_from_code = xlrd.biffh.error_text_from_code
+            hyperlink_map = sheet.hyperlink_map
+            cell_note_map = sheet.cell_note_map
+            empty_cell_types = (xlrd.XL_CELL_EMPTY, xlrd.XL_CELL_BLANK)
 
             for row in range(rows):
                 for col in range(columns):
                     cell_type = sheet.cell_type(row, col)
                     value = sheet.cell_value(row, col)
+                    tup = (row, col)
+                    hyperlink_info = hyperlink_map.get(tup)
+                    comment = cell_note_map.get(tup)
+                    font, fill, border, alignment, number_format, protection, is_default_style = self.xls_style_to_xlsx(sheet.cell_xf_index(row, col))
+
+                    # Skip creating truly empty cells that carry no style/metadata.
+                    if cell_type in empty_cell_types and is_default_style and hyperlink_info is None and comment is None:
+                        continue
+
                     if cell_type == xlrd.XL_CELL_DATE:
                         try:        # Issue #5: Just keep 'bad' dates as float numbers
                             value = self.xls_date_to_xlsx(value)
@@ -243,26 +290,23 @@ class XLS2XLSX:
                         except Exception:
                             pass
                     elif cell_type == xlrd.XL_CELL_ERROR:
-                        if value in xlrd.biffh.error_text_from_code:
-                            value = xlrd.biffh.error_text_from_code[value]
+                        if value in error_text_from_code:
+                            value = error_text_from_code[value]
                         else:
                             value = '#N/A'
-                    elif cell_type in (xlrd.XL_CELL_EMPTY, xlrd.XL_CELL_BLANK):
-                        value = ''
+                    elif cell_type in empty_cell_types:
+                        value = None
                     elif cell_type == xlrd.XL_CELL_BOOLEAN:
                         value = ('false', 'true')[value]
 
                     rw = row+1
                     cc = col+1
-                    ws.cell(rw, cc).value = value
-                    font, fill, border, alignment, number_format, protection = self.xls_style_to_xlsx(sheet.cell_xf_index(row, col))
+                    cell = ws.cell(rw, cc, value=value)
                     #if number_format != 'General':
                         #print(f'({rw},{cc}).number_format = {number_format}')
-                    if number_format is None:
-                        number_format = 'General'
                     if isinstance(value, str):
                         if '\n' in value and not alignment.wrap_text:
-                            alignment = copy.deepcopy(alignment)
+                            alignment = copy.copy(alignment)
                             alignment.wrap_text = True
                         if value[-1:] == '%' and number_format == 'General':
                             number_format = numbers.FORMAT_PERCENTAGE
@@ -294,24 +338,21 @@ class XLS2XLSX:
                     elif isinstance(value, timedelta):
                         if number_format == 'General':
                             number_format = '[h]:mm:ss'
-                    ws.cell(rw, cc).font = font
-                    ws.cell(rw, cc).fill = fill
-                    ws.cell(rw, cc).border = border
-                    ws.cell(rw, cc).alignment = alignment
+                    cell.font = font
+                    cell.fill = fill
+                    cell.border = border
+                    cell.alignment = alignment
                     #if number_format != 'General':
                         #print(f'({rw},{cc}).number_format = {number_format}, .value = {value}, type = {type(value)}')
-                    ws.cell(rw, cc).number_format = number_format
-                    ws.cell(rw, cc).protection = protection
+                    cell.number_format = number_format
+                    cell.protection = protection
                     if protection.locked or protection.hidden:
                         ws.protection.sheet = True
-                    tup = (row, col)
-                    if tup in sheet.hyperlink_map:
-                        hyperlink = sheet.hyperlink_map[tup].url_or_path
-                        ws.cell(rw, cc).hyperlink = hyperlink
+                    if hyperlink_info is not None:
+                        cell.hyperlink = hyperlink_info.url_or_path
                         #ws.cell(rw, cc).style = 'Hyperlink'
-                    if tup in sheet.cell_note_map:
-                        comment = sheet.cell_note_map[tup]
-                        ws.cell(rw, cc).comment = Comment(comment.text, comment.author)
+                    if comment is not None:
+                        cell.comment = Comment(comment.text, comment.author)
                     image = False       # FIXME (after fixing xlrd)
                     if image:
                         image.anchor = f'{cl}{rw}'
